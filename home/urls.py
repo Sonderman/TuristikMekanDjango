927cf3d1d1aa60a60ddf62ddac386b290bf9a797
Sonderman <alihaydar338@gmail.com>
2020-08-04 23:36:45 +0300

    ***Hakkımızda ve Referanslar sayfaları yapıldı.

--- home/urls.py
+++ home/urls.py
@@ -4,5 +4,6 @@ from . import views
 
 urlpatterns = [
     path('', views.index, name='index'),
-
+    path('aboutus', views.aboutus, name='aboutus'),
+    path('references', views.references, name='references'),
 ]
--- home/views.py
+++ home/views.py
@@ -11,3 +11,21 @@ def index(request):
         "setting": setting
     }
     return render(request, 'index.html', context)
+
+
+def aboutus(request):
+    setting = Setting.objects.get(pk=1)
+    # category = Category.objects.all()
+    context = {
+        "setting": setting
+    }
+    return render(request, 'hakkimizda.html', context)
+
+
+def references(request):
+    setting = Setting.objects.get(pk=1)
+    # category = Category.objects.all()
+    context = {
+        "setting": setting
+    }
+    return render(request, 'referanslar.html', context)
