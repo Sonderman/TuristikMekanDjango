540d6ca7e4f82788977486346744bc936f395e97
Sonderman <alihaydar338@gmail.com>
2020-08-06 12:17:02 +0300

    *** Category ve Place modelleri oluşturuldu.

--- mekan/admin.py
+++ mekan/admin.py
@@ -1,3 +1,66 @@
 from django.contrib import admin
+from mptt.admin import DraggableMPTTAdmin
 
-# Register your models here.
+from mekan.models import *
+
+
+class ImageInline(admin.TabularInline):
+    model = Images
+    extra = 5
+
+
+class PlaceAdmin(admin.ModelAdmin):
+    list_display = ['title', 'category', 'image_tag', 'status']
+    readonly_fields = ('image_tag',)
+    list_filter = ['category']
+    inlines = [ImageInline]
+    prepopulated_fields = {'slug': ('title',)}
+
+
+class ImagesAdmin(admin.ModelAdmin):
+    list_display = ['title', 'place', 'image_tag']
+    readonly_fields = ('image_tag',)
+
+
+class MPTTCategoryAdmin(DraggableMPTTAdmin):
+    mptt_indent_field = "title"
+    readonly_fields = ('image_tag',)
+    list_filter = ['status']
+    list_display = ('tree_actions', 'indented_title',
+                    'related_products_count', 'related_products_cumulative_count', 'image_tag', 'status')
+    list_display_links = ('indented_title',)
+    prepopulated_fields = {'slug': ('title',)}
+
+    def get_queryset(self, request):
+        qs = super().get_queryset(request)
+
+        # Add cumulative product count
+        qs = Category.objects.add_related_count(
+            qs,
+            Place,
+            'category',
+            'products_cumulative_count',
+            cumulative=True)
+
+        # Add non cumulative product count
+        qs = Category.objects.add_related_count(qs,
+                                                Place,
+                                                'category',
+                                                'products_count',
+                                                cumulative=False)
+        return qs
+
+    def related_products_count(self, instance):
+        return instance.products_count
+
+    related_products_count.short_description = 'Related products (for this specific category)'
+
+    def related_products_cumulative_count(self, instance):
+        return instance.products_cumulative_count
+
+    related_products_cumulative_count.short_description = 'Related products (in tree)'
+
+
+admin.site.register(Category, MPTTCategoryAdmin)
+admin.site.register(Place, PlaceAdmin)
+admin.site.register(Images, ImagesAdmin)
--- mekan/models.py
+++ mekan/models.py
@@ -1,3 +1,112 @@
+from ckeditor.widgets import CKEditorWidget
+from ckeditor_uploader.fields import RichTextUploadingField
+from django.contrib.auth.models import User
 from django.db import models
+from django.forms import ModelForm, TextInput, Select, FileInput
+from django.urls import reverse
+from django.utils.safestring import mark_safe
+from mptt.fields import TreeForeignKey
+from mptt.models import MPTTModel
 
-# Create your models here.
+
+class Category(MPTTModel):
+    STATUS = (
+        ('True', 'Evet'),
+        ('False', 'Hayır'),
+    )
+
+    title = models.CharField(max_length=30)
+    keywords = models.CharField(max_length=255)
+    description = models.CharField(max_length=255)
+    CategoryImage = models.ImageField(blank=True, upload_to='assets/images/')  # py -m pip install --upgrade Pillow
+    status = models.CharField(max_length=10, choices=STATUS)
+    slug = models.SlugField(null=False, unique=True)
+    parent = TreeForeignKey('self', blank=True, null=True, related_name='children', on_delete=models.CASCADE)
+    create_at = models.DateTimeField(auto_now_add=True)
+    update_at = models.DateTimeField(auto_now=True)
+
+    class MPTTMeta:
+        # level_attr = 'mptt_level'
+        order_insertion_by = ['title']
+
+    def __str__(self):
+        full_path = [self.title]
+        k = self.parent
+        while k is not None:
+            full_path.append(k.title)
+            k = k.parent
+        return ' / '.join(full_path[::-1])
+
+    def image_tag(self):
+        return mark_safe('<img src="{}" height="50"/>'.format(self.CategoryImage.url))
+
+    image_tag.short_description = 'Image'
+
+    def get_absolute_url(self):
+        return reverse('category_detail', kwargs={'slug': self.slug})
+
+
+class Place(models.Model):
+    STATUS = (
+        ('True', 'Evet'),
+        ('False', 'Hayır'),
+    )
+    user = models.ForeignKey(User, on_delete=models.SET_NULL, null=True)
+    category = models.ForeignKey(Category, on_delete=models.CASCADE)
+    title = models.CharField(max_length=100)
+    keywords = models.CharField(max_length=255)
+    description = models.CharField(max_length=255)
+    detail = RichTextUploadingField(blank=True)
+    slug = models.SlugField(null=False, unique=True)
+    image = models.ImageField(upload_to='images/', max_length=255)  # py -m pip install --upgrade Pillow
+    status = models.CharField(max_length=10, choices=STATUS)
+    create_at = models.DateTimeField(auto_now_add=True)
+    update_at = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return self.title
+
+    def image_tag(self):
+        return mark_safe('<img src="{}" height="50"/>'.format(self.image.url))
+
+    image_tag.short_description = 'Image'
+
+    def get_absolute_url(self):
+        return reverse('place_detail', kwargs={'slug': self.slug})
+
+
+class PlaceForm(ModelForm):
+    class Meta:
+        model = Place
+        fields = ['category', 'title', 'slug', 'keywords', 'description', 'image', 'detail']
+        widgets = {
+            'title': TextInput(attrs={'class': 'input', 'placeholder': 'title'}),
+            'keywords': TextInput(attrs={'class': 'input', 'placeholder': 'keywords'}),
+            'description': TextInput(attrs={'class': 'input', 'placeholder': 'description'}),
+            'category': Select(attrs={'class': 'input', 'placeholder': 'type'}, choices=(
+                Category.objects.all()
+            )),
+            'image': FileInput(attrs={'class': 'input', 'placeholder': 'image'}),
+            'slug': TextInput(attrs={'class': 'input', 'placeholder': 'slug'}),
+            'detail': CKEditorWidget(),
+        }
+
+
+class Images(models.Model):
+    place = models.ForeignKey(Place, on_delete=models.CASCADE)
+    title = models.CharField(max_length=50, blank=True)
+    image = models.ImageField(blank=True, upload_to='images/', max_length=255)  # py -m pip install --upgrade Pillow
+
+    def __str__(self):
+        return self.title
+
+    def image_tag(self):
+        return mark_safe('<img src="{}" height="50"/>'.format(self.image.url))
+
+    image_tag.short_description = 'Image'
+
+
+class ImagesForm(ModelForm):
+    class Meta:
+        model = Images
+        fields = ['title', 'image']
